bb56f9fe7a8df894358c9c165ee3bcd9154243bf
Bhagwat Chouhan <bhagwat.chouhan@gmail.com>
2017-08-16 14:13:58 +0530

    Initial commit.

---
+++ migrations/innodb/db-drop.sql
@@ -0,0 +1,25 @@
+USE `cmgdemo`;
+
+SET FOREIGN_KEY_CHECKS=0;
+
+/* ============================= CMSGears CMS ============================================== */
+
+DROP TABLE IF EXISTS `cmg_cms_menu`;
+
+DROP TABLE IF EXISTS `cmg_cms_page`;
+
+DROP TABLE IF EXISTS `cmg_cms_menu_page`;
+
+DROP TABLE IF EXISTS `cmg_cms_page_meta`;
+
+DROP TABLE IF EXISTS `cmg_cms_page_file`;
+
+DROP TABLE IF EXISTS `cmg_cms_post_category`;
+
+DROP TABLE IF EXISTS `cmg_cms_sidebar`;
+
+DROP TABLE IF EXISTS `cmg_cms_widget`;
+
+DROP TABLE IF EXISTS `cmg_cms_sidebar_widget`;
+
+SET FOREIGN_KEY_CHECKS=1;
