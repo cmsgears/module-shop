e41f2a875e9bfe544c20e44aa6000f8f4a009e30
Bhagwat Singh Chouhan <bhagwat.chouhan@vulpinecode.com>
2018-06-16 11:06:56 +0530

    Applied quick fixes.

--- common/config/ShopGlobal.php
+++ common/config/ShopGlobal.php
@@ -31,7 +31,6 @@ class ShopGlobal {
 	const TEMPLATE_CART		= 'cart';
 	const TEMPLATE_CHECKOUT	= 'checkout';
 	const TEMPLATE_PAYMENT	= 'payment';
-	const TEMPLATE_SHOP		= 'shop';
 
 	// Config ----------------------------------------------------------
 
